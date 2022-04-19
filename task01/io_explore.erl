24008bc8f90af2768c69aab9eede285c710f7c66
Denis Lazo <denislazo1610@gmail.com>
2022-04-19 08:07:29 -0600

    Week 01, list comphrension, still working on

--- task01/io_explore.erl
+++ task01/io_explore.erl
@@ -2,10 +2,10 @@
 -export([print_greeting_nice/0, print_greeting_strange/0]).
 
 print_greeting_nice()->
-    Name = io:get_line("Hello, what's your name? "),
-    io:format("It is so nice to meet you ~s~n",[Name]).
+    Nombre = io:get_line("What's your name? "),
+    io:format("It is so nice to meet you ~s~n",[Nombre]).
 
 print_greeting_strange()->
-    Name = io:get_line("Hello, what's your name? "),
-    io:format("It is so nice to meet you ~w~n",[Name]).
+    Nombre = io:get_line("What's your name? "),
+    io:format("It is so nice to meet you ~w~n",[Nombre]).
 
